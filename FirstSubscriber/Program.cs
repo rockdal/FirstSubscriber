45c206225f321168dc60a59760e2a72629f86d25
Anthoney Hanks <hanksjt@gmail.com>
2022-02-23 20:54:18 -0600

    Initial Load

---
+++ FirstSubscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+
+namespace FirstSubscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Bind("tcp://localhost:5555");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Sending Hello");
+                    client.SendFrame("Hello");
+                    var message = client.ReceiveFrameString();  
+                    Console.WriteLine("Received {0}", message);
+                }
+            }
+         }
+    }
+}
